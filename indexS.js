a085e3c5636d0f921419906d83e8e7b029c7cc21
Alex <alextravc.342@gmail.com>
2021-10-05 18:31:33 -0500

    initial setup

---
+++ indexS.js
@@ -0,0 +1,26 @@
+/** Arquitectura de una API
+ * s
+	- Instalar dependencias
+		npm i express express-async-errors awilix cors compression helmet bcryptjs jsonwebtoken memory-cache mongoose swagger-ui-express
+			- express: Servidor
+			- ex-as-err: Manjar errores async
+			- awilix: Container servicio de dependecias
+			- cors: Peticiones al exterior desde nuestra API
+			- compression: Comprimir peticiones HTTP
+			- helmet: Seguridad
+			- bcrypt: encriptar las contrasenas
+			- jsonwebtoken: Generar los tokens
+			- memory-cache: cache
+			- swagger: Documentar la API
+
+		npm i -D dotenv nodemon mockingoose jest
+
+
+	- Creamos la estructura de N capas: Variso folders
+		-  
+
+	- 
+
+ * s
+ * s
+ */
